00d980412bdc8f0a78567a0fcc1fa9f306efc76c
Sh1tters <justinbestworldwide@gmail.com>
2022-11-21 15:31:56 +0100

    Foundation Created

---
+++ Game.py
@@ -0,0 +1,14 @@
+def preload():
+    """ This function essentially loads things into our game memory """
+    pass
+
+def create():
+    """ This function is relatively straightforward - runs once at the beginning of the game and displays the loaded things from preload() """
+    pass
+
+def update():
+    """ This function will run constantly """
+    pass
+
+if __name__ == '__main__':
+    pass
---
+++ phaser.min.py
@@ -0,0 +1,5 @@
+""" Phaser file with default start off variables stored as the configuration for the game """
+
+width = 512
+height = 544
+backgroundColor: 0x000000
