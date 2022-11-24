0aad737bd94b9f986b9e1ca8e2e8ff32a3652119
Sh1tters <justinbestworldwide@gmail.com>
2022-11-24 19:57:44 +0100

    Minor Changes

--- Game.py
+++ Game.py
@@ -1,23 +1,23 @@
 import pygame
 from config import *
+from Player import Player
     
 pygame.init()    
-screen = pygame.display.set_mode((width, height))    
+surface = pygame.display.set_mode((width, height))    
 clock = pygame.time.Clock()  
 game_running = True
-scene = 0
-"""
-    scene: 0 # Menu
-    scene: 1 # Game
-"""
-    
+player = Player(surface)
+
 while game_running:    
     clock.tick(frameRate) # FPS
 
     # Background
-    background = pygame.Surface(screen.get_size())
+    background = pygame.Surface(surface.get_size())
     background.fill(backgroundColor)
-    screen.blit(background, (0, 0))
+    surface.blit(background, (0, 0))
+
+    # Update Player
+    player.updatePlayer()
     
     for event in pygame.event.get():    
         if event.type == pygame.QUIT:    
